42e84ff8f19b7d51b6496d8368c1af64115d37a4
babydmg <126503300+babydmg@users.noreply.github.com>
2023-10-31 20:36:52 +0500

    Navbar + Footer Complete

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -7,14 +7,14 @@ const CustomButton = ({
   title,
   containerStyles,
   handleClick,
+  btnType,
 }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={'button'}
+      type={btnType || 'button'}
       className={`custom-btn ${containerStyles}`}
-      onClick={handleClick}
-    >
+      onClick={handleClick}>
       <span className={`flex-1`}>{title}</span>
     </button>
   );
